5ecd5cf0367a453c22c970422c9877971a5a9bce
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-08-08 16:58:27 -0300

    feat: resolução exercício do robô

--- script05.js
+++ script05.js
@@ -17,3 +17,24 @@ console.log("pizza montada:");
 for (let i = 0; i < pizza.length; i++) {
     console.log(pizza[i]);
 }
+
+
+let armas = ["sniper", "fuzil", "AK47", "MP40","magnum"];
+let ataques = [];
+
+ataques.push("leleo");
+ataques.push(armas[0]);
+ataques.push(armas[1]);
+ataques.push(armas[2]);
+ataques.push(armas[3]);
+ataques.push(armas[4]);
+
+console.log("nome:leleo");
+
+for (let i = 0; i < ataques.length; i++) {
+    if (i == 0) {
+        console.log("nome do rôbo:" + ataques[i]);
+    } else {
+        console.log("ataque " + i + ":" + ataques[i]);
+    }
+}
